030d82d026d1fb247517d65bb899dc25dce99734
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-31 23:03:24 -0500

    write fixed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """  Python script to export data in the CSV format. """
+import csv
 import requests
 from sys import argv
 
@@ -15,20 +16,25 @@ def export_to_csv():
     users = requests.get("http://jsonplaceholder.typicode.com/users")
     for u in users.json():
         if u.get('id') == int(argv[1]):
-            USER_ID = (u.get('id'))
             USERNAME = (u.get('username'))
             break
-    TASK_COMPLETED_STATUS = []
-    TASK_TITLE = []
+    TASK_STATUS_TITLE = []
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
     for t in todos.json():
         if t.get('userId') == int(argv[1]):
-            TASK_COMPLETED_STATUS.append(t.get('completed'))
-            TASK_TITLE.append(t.get('title'))
-    print("{}, {}, {}, {}".format(USER_ID, USERNAME,
-          TASK_COMPLETED_STATUS, TASK_TITLE))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
+            TASK_STATUS_TITLE.append(t.get('completed'), t.get('title'))
+
+    filename = "{}.csv".format(argv[1])
+    with open(filename, mode='w') as csv_file:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(
+            csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+
+        for task in TASK_STATUS_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                            "TASK_COMPLETED_STATUS": task[0],
+                             "TASK_TITLE": task[1]})
 
 
 if __name__ == "__main__":
