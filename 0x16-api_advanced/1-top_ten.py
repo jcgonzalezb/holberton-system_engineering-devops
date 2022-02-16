2eba1769fbce8df314bd25bbde8b92019070bfb4
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 22:02:05 -0500

    parameters fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ first 10 hot posts listed for a given subreddit.
 """
 import requests
 
-URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+URL = 'https://www.reddit.com/r/{}/hot.json'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
 
 
@@ -16,6 +16,7 @@ def top_ten(subreddit):
     resp = requests.get(
         URL.format(subreddit),
         headers={'User-Agent': USER_AGENT},
+        params={'limit': 10},
         allow_redirects=False,
         timeout=10
     )
