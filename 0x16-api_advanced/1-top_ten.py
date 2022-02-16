67762b3bd57fcf44c59b48844826490581ec5b02
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 21:20:22 -0500

    URL fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ first 10 hot posts listed for a given subreddit.
 """
 import requests
 
-URL = 'https://www.reddit.com/r/{}/top.json'
+URL = 'https://www.reddit.com/r/{}/{}.json?limit={}&t={}'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
 limit = 10
 timeframe = 'all'
