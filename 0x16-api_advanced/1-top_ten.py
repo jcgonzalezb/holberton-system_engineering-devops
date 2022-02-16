4cb58f2e96085837094032dbcc4c962377631924
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 21:56:22 -0500

    syntax fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,8 @@ first 10 hot posts listed for a given subreddit.
 """
 import requests
 
-URL = 'https://www.reddit.com/r/{}/{}.json?limit={}'
+URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
-listing = 'hot'
-limit = 10
 
 
 def top_ten(subreddit):
@@ -16,7 +14,7 @@ def top_ten(subreddit):
     Query reddit for the titles of the first 10 hot posts listed for a given subreddit.
     """
     resp = requests.get(
-        URL.format(subreddit, listing, limit),
+        URL.format(subreddit),
         headers={'User-Agent': USER_AGENT},
         allow_redirects=False,
         timeout=10
