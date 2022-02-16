a9dbf41f11eaa7089fdbf9b1acf43549a3b17896
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 21:17:11 -0500

    print fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns
-the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
+Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
 """
-
 import requests
 
-URL = 'https://www.reddit.com/r/{}/about.json'
+URL = 'https://www.reddit.com/r/{}/top.json'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
+limit = 10
+timeframe = 'all'
+listing = 'top'
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """
-    Query reddit for the number of subscribers to a subreddit
+    Query reddit for the titles of the first 10 hot posts listed for a given subreddit.
     """
     resp = requests.get(
-        URL.format(subreddit),
+        URL.format(subreddit, listing, limit, timeframe),
         headers={'User-Agent': USER_AGENT},
         allow_redirects=False,
         timeout=10
     )
+
     if resp.status_code == 200:
-        return resp.json()['data']['subscribers']
+        r = resp.json()
+        for post in r['data']['children']:
+            print(post['data']['title'])
     else:
-        return 0
+        return None
