7241158aaea6f7506e798851d9ccbed0adc29c22
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-16 14:31:35 -0500

    Initial version

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+URL = 'https://www.reddit.com/r/{}/{}.json?limit={}'
+USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
+listing = 'hot'
+limit = 10
+
+
+def top_ten(subreddit):
+    """
+    Query reddit for the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+
+    resp = requests.get(
+        URL.format(subreddit, listing, limit),
+        headers={'User-Agent': USER_AGENT},
+        allow_redirects=False,
+        timeout=10
+    )
+    if resp.status_code == 200:
+        for post in resp.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
