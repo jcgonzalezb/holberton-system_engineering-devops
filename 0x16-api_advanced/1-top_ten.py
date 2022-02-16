8040eb6c70fc5961e7d25908359839a65ee4d723
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 22:19:33 -0500

    PEP8 fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and prints the titles of the
+Function that queries the Reddit API and prints the titles of the
 first 10 hot posts listed for a given subreddit.
 """
 import requests
 
+URL = 'https://www.reddit.com/r/{}/{}.json?limit={}'
+USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
+listing = 'hot'
+limit = 10
+
 
 def top_ten(subreddit):
     """
-    Query reddit for the titles of the first 10 hot posts listed for a given subreddit.
+    Query reddit for the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
-    URL = 'https://www.reddit.com/r/{}/hot.json'
-    USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
 
     resp = requests.get(
-        URL.format(subreddit),
+        URL.format(subreddit, listing, limit),
         headers={'User-Agent': USER_AGENT},
-        params={'limit': 10},
         allow_redirects=False,
         timeout=10
     )
@@ -24,4 +27,4 @@ def top_ten(subreddit):
         for post in resp.json()['data']['children']:
             print(post['data']['title'])
     else:
-        print('None')
+        print(None)
