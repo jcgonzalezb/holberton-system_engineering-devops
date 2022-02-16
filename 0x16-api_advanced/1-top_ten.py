3e5fad7840ec25402425d32aa44463e9e176a64b
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-15 21:54:27 -0500

    return deleted and parameters fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,8 +5,10 @@ first 10 hot posts listed for a given subreddit.
 """
 import requests
 
-URL = 'https://www.reddit.com/r/{}/hot.json'
+URL = 'https://www.reddit.com/r/{}/{}.json?limit={}'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
+listing = 'hot'
+limit = 10
 
 
 def top_ten(subreddit):
@@ -14,9 +16,8 @@ def top_ten(subreddit):
     Query reddit for the titles of the first 10 hot posts listed for a given subreddit.
     """
     resp = requests.get(
-        URL.format(subreddit),
+        URL.format(subreddit, listing, limit),
         headers={'User-Agent': USER_AGENT},
-        params={'limit': 10},
         allow_redirects=False,
         timeout=10
     )
@@ -25,4 +26,4 @@ def top_ten(subreddit):
         for post in resp.json()['data']['children']:
             print(post['data']['title'])
     else:
-        return print("None")
+        print('None')
